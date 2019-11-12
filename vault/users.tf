272d25feea15c4fdd7706c916465063c2b5e2d7d
Jeremy Kitchen <kitchen@kitchen.io>
2019-11-12 09:14:55 -0800

    TODO for vault user module

--- vault/users.tf
+++ vault/users.tf
@@ -1,3 +1,12 @@
+# TODO: extract this out as a module somehow?
+# my only concern really is that groups seem to need the complete list of members
+# so how do I do that while creating users with modules? :shrug:
+
+# alternatively, if vault's ACL stuff allowed for templating in the parameters
+# bits, we could make it so there's a single ssh ca role that allows all
+# usernames (read: principals) to be specified, but the ACL limits it to the
+# identity's name. Maybe. That's kinda what I was hoping for, anyways :)
+
 resource "vault_identity_entity" "kitchen" {
   name     = "kitchen"
   policies = ["default"]
